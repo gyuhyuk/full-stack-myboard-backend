51fa49d8998765eedc406757e4b3b636dc7d98b0
gyuhyuk <bruno3@naver.com>
2024-07-11 00:16:52 +0900

    feat : Article 도메인 생성자 및 동등성 검사 추가

--- src/main/java/com/myboard/myboard/article/Article.java
+++ src/main/java/com/myboard/myboard/article/Article.java
@@ -1,13 +1,56 @@
 package com.myboard.myboard.article;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
-    private Long id; // PK
-    private String title; // 제목
-    private String content; // 본문
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; // 생성일
-    private String createdBy; // 생성자
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // PK
+
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+    @Setter private String hashtag; // 해시태그
+
+    private LocalDateTime createdAt; // TODO : BaseEntity 생성 시 변경
+    private String createdBy; // TODO : Member 도메인 생성 시 Member로 변경
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
