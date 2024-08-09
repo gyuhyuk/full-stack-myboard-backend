70304e456e88b9aac41891674babded62521a73d
gyuhyuk <bruno3@naver.com>
2024-08-09 21:29:33 +0900

    feat : ArticleService 추가

---
+++ src/main/java/com/myboard/myboard/article/service/ArticleService.java
@@ -0,0 +1,13 @@
+package com.myboard.myboard.article.service;
+
+import com.myboard.myboard.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+}
